07fa8d809d0ba2fdfeeebd357a74de9d8c6dfb68
Krzysztof Góral <80343@student.pb.edu.pl>
2022-11-15 23:21:57 +0100

    ...

--- GastroFaza/Models/DTO/CreateWorkerDto.cs
+++ GastroFaza/Models/DTO/CreateWorkerDto.cs
@@ -8,31 +8,22 @@ namespace GastroFaza.Models.DTO
         [Required]
         [EmailAddress]
         public string Email { get; set; } = null!;
-        
         [Required]
         public string FirstName { get; set; } = "";
-        
         [Required]
         public string LastName { get; set; } = "";
-        
         [Required]
         public DateTime? DateOfBirth { get; set; }
-        
         [Required]
         public string Nationality { get; set; } = "";
-        
         [Required]
         public string PasswordHash { get; set; } = null!;
-        
         [Required]
         public float Salary { get; set; }
         [Required]
         public int Rating { get; set; }
-
         [Required] 
         public int RoleId { get; set; }
 
-        [Required] 
-        public virtual Role Role { get; set; } = null!;
     }
 }
