21fb675e925f16e233ad07df89cf94d45ac59d05
Krzysztof Góral <80343@student.pb.edu.pl>
2022-11-17 15:02:19 +0100

    ???

---
+++ GastroFaza/Migrations/20221115183450_First-Init-Server.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GastroFaza.Migrations
+{
+    public partial class FirstInitServer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
