f391332b45fe70968466edcc988df5fed7414c84
Krzysztof Góral <80343@student.pb.edu.pl>
2022-11-17 14:40:23 +0100

    Init change

--- GastroFaza/Migrations/20221117133710_Init.Designer.cs
+++ GastroFaza/Migrations/20221117133710_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GastroFaza.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    [Migration("20221117093250_Init")]
+    [Migration("20221117133710_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
