a2f43959766912178e1ee33a3ee21cd5660b8b87
Lethalman777 <karbosky7@gmail.com>
2022-11-30 12:37:19 +0100

    popraw plissss

--- GastroFaza/Controllers/OrderController.cs
+++ GastroFaza/Controllers/OrderController.cs
@@ -16,13 +16,33 @@ namespace GastroFaza.Controllers
         [Route("Order")]
         public IActionResult Order()
         {
-            IEnumerable<Order> orders = this.dbContext.Orders;
-            return View(orders);
+            Order order = this.dbContext.Orders.FirstOrDefault();
+            //if(order.Dishes.Count == 0)
+            //    return RedirectToAction("GetAll", "Dish");
+            HttpContext.Session.SetString("current order", order.Id.ToString());
+            return View(order);
+        }
+        public IActionResult RemoveDishFromOrder(Order order, Dish dish)
+        {
+            order.Dishes.Remove(dish);
+            this.dbContext.Orders.FirstOrDefault(u=>u.Id==order.Id).Dishes.Remove(dish);
+            try
+            {
+                this.dbContext.SaveChanges();
+                HttpContext.Session.SetString("current order", this.dbContext.Orders.FirstOrDefault(u => u == order).Id.ToString());
+            }
+            catch (DbUpdateException e)
+            {
+                throw new DbUpdateException("Error DataBase", e);
+            }
+            return View();
         }
 
         [Route("Create")]
         public IActionResult Create()
         {
+            //if(this.dbContext.Orders.ToList().Count > 0)
+            //this.dbContext.Orders.Remove(this.dbContext.Orders.FirstOrDefault(u=>u.Id==int.Parse(HttpContext.Session.GetString("current order"))));
             var order = new Order()
             {
                 Description = "",
