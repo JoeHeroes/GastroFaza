f4abcb9ed7e382d91e901d1c7a4d194c55df9bfa
Dawid Gramacki <80342@student.pb.edu.pl>
2023-04-23 18:57:46 +0200

    Front

---
+++ GastroFaza/wwwroot/css/JavaScript.js
@@ -0,0 +1,6 @@
+﻿const selectList = document.getElementById("nationality");
+const selectedValue = document.querySelector(".select_list_nationality .selected-value");
+
+selectList.addEventListener("change", () => {
+    selectedValue.textContent = selectList.value;
+});
